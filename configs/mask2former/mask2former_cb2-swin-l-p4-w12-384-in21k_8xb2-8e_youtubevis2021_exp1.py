d4f3b65b8e40e6ee42cc247da9d2ecd1c47fa813
chuxiaojie <chuxiaojie@stu.pku.edu.cn>
2023-05-13 10:18:24 +0800

    upd cb-swin-l config

---
+++ configs/mask2former/mask2former_cb2-swin-l-p4-w12-384-in21k_8xb2-8e_youtubevis2021_exp1.py
@@ -0,0 +1,1 @@
+_base_ = ['mask2former_cb2-swin-l-p4-w12-384-in21k_8xb2-8e_youtubevis2021.py']
--- test_vis.sh
+++ test_vis.sh
@@ -1,5 +1,5 @@
 NUM_GPU=1
 # bash video_tracking/tools/dist_test.sh $1 $NUM_GPU --checkpoint $2 --cfg-options model.track_head.pixel_decoder.encoder.layer_cfg.self_attn_cfg.im2col_step=1
 # bash video_tracking/tools/dist_test.sh video_tracking/configs/vis/mask2former/$1.py $NUM_GPU --checkpoint $2 --cfg-options model.track_head.pixel_decoder.encoder.layer_cfg.self_attn_cfg.im2col_step=1 val_dataloader.outfile_prefix=$1 test_evaluator.outfile_prefix=$1
-bash video_tracking/tools/dist_test.sh configs/mask2former/$1.py $NUM_GPU --checkpoint $2 --cfg-options model.track_head.pixel_decoder.encoder.layer_cfg.self_attn_cfg.im2col_step=1 val_dataloader.outfile_prefix=$1 test_evaluator.outfile_prefix=$1
+bash video_tracking/tools/dist_test.sh configs/mask2former/$1.py $NUM_GPU --checkpoint work_dirs/$1/iter_8000.pth --cfg-options model.track_head.pixel_decoder.encoder.layer_cfg.self_attn_cfg.im2col_step=1 val_dataloader.outfile_prefix=$1 test_evaluator.outfile_prefix=$1 model.init_cfg=None model.backbone.init_cfg=None test_cfg.fp16=true
 
